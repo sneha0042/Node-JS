ef4590b34128bb900962544d29e6ceca89a7814d
sneha0042 <snehabandari07@gmail.com>
2024-07-13 00:16:58 +0530

    Add files via upload

---
+++ index_operators.js
@@ -0,0 +1,69 @@
+let string1='Sneha'
+console.log(string1, typeof string1)
+
+let num=21
+console.log(num,typeof num)
+
+let bool=true
+console.log(bool, typeof bool)
+
+let undef
+console.log(undef,typeof undef)
+
+let i=null
+console.log(a,typeof i)
+
+let a=20
+let b=10
+//addition
+console.log("addition",a+b)
+//subraction
+console.log("subraction",a-b)
+//multiplication
+console.log("multiplication",a*b)
+//modulus
+console.log("modulus",a%b)
+//division
+console.log("division",a/b)
+//equal
+console.log("equal",a=b)
+
+let k=30
+let l=20
+
+console.log("==", x==y)
+
+console.log("===", x===y)
+
+console.log("not equal !=", x!=y)
+
+console.log("not equal !==", x!==y)
+
+console.log("greater than", x>y)
+
+console.log("less than", x<y)
+
+console.log("greater than or equal to", x>=y)
+
+console.log("less than or equal to", x<=y)
+
+
+//output
+/*Sneha string
+21 number
+true boolean
+undefined undefined
+null object
+sum 30
+sub 10
+mul 50
+div 2
+mod 0
+== true
+=== false
+not equal false
+not equal true
+greater than false
+less than false
+greater than or equal to true
+less than or equal to true*/
